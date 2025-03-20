790df97a8704b7bf6242a8fb8060dffd7e873274
24LP7422 24LP7422 <24LP7422@G302-CC0115.local>
2025-03-20 11:14:06 +0800

    mapp

---
+++ src/app/page.tsx
@@ -0,0 +1,170 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Movies } from "@/components/Movies";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { ChevronDown } from "lucide-react";
+import { Film } from "lucide-react";
+import { Search } from "lucide-react";
+import { Moon } from "lucide-react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+
+const upcomingData = [
+  {
+    image:
+      "https://s3-alpha-sig.figma.com/img/c78e/5e57/16d36abbdaa8df480db189d5729e384a?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=mLG2xOJZNT2vGCrvhDwo3-iKl0QLHOJCxUbykqD81OWYT61RDmWH~sY5qc4qVMmDHRoLdT3VXAnqpxjl4QRY7lvqwtvVTj2-RScRPADrSRE2X1dKJ6MNwI89GQsAr7CVA~Sw886s4cN3GzZCxbhX6nG5wCcsdExQ3ZifH-DrPK1y2qNpWDmJzamRmYUQB4G5gKUvdNeqjPEES5nuyWmp4tVWbJDWV1Ve6DECdtwn6WwE~0puD445Fe7qQpsvTO15bYmHP3E7sN6ZamI~BBe1H7Aisb1JjhHE35MH~r0CHClF6Ayy8aDTsnbuKYmE-rzkB3IlXZLoaZaJNRHFDc~Erg__",
+    title: "Wicked",
+    description: "Now Playing:",
+    score: "6.9/10",
+    content:
+      "Elphaba, a misunderstood young woman because of her green skin, and Glinda, a popular girl, become friends at Shiz University in the Land of Oz. After an encounter with the Wonderful Wizard of Oz, their friendship reaches a crossroads. ",
+    trailer: "1",
+  },
+  {
+    image:
+      "https://s3-alpha-sig.figma.com/img/d5db/f97e/32f19c86b63dc76ec6e6f1a31cfd4d28?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=GQ3meWR9a8frPgpoOEvHvaeeOEnpTNjgnnY0dCC5CxKS0G6TT7E4tIxkNK6Pe5C-X8Cja1D6uYhmu0tryE~~E7n3tM-3F~byxoal5V85BU-qUbWpEGD-aBBBKUgN65PYW3n0EZyuyENF6MhZDk9JYvu~ILSh5ffE~mIxkvSIHSg2zPbFCSTgYaSlWcU3~7AcUoC0vAQkE1wskI5RGR5Os9B-nHlHx3WFI06aEjUtrGWrnOtITztzJhxia1kzv7zWSNTs4PVX8RbAA09EPuX1WMk4fyHrihyHRkmaYuAc7u9EmbDEdieypHtoxUK2kY9bJUu6AFV9HJ3UmDyPU0GVEg__",
+    title: "Gladiator II",
+    description: "Now Playing:",
+    score: "6.9/10",
+    content:
+      "After his home is conquered by the tyrannical emperors who now lead Rome, Lucius is forced to enter the Colosseum and must look to his past to find strength to return the glory of Rome to its people.",
+    trailer: "1",
+  },
+  {
+    image:
+      "https://s3-alpha-sig.figma.com/img/496c/7ace/9382b0cceb0901c79ace811454bb1211?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=jzW6r7JpZfn3xFX62iky5fm7obx7D8s6jRUtlzqcYFXShjBXKx0Nqexsg3hKQ~g9cbsadr1nqxcXkIVp9m0VsCSRL3BAxtPx82iuSdx5i-OT7vKn2OiYlByLxy7mpQltYMp4S9k4cJlP0sF2-tG8mcyC87BI-YjP2JmRzb9xco5qOG0FswjRbk8iuFZAtKkRW9MoMyN6xo6I9WNfpak2B8Xw2e0ykkKiqbw7JMe9WUUstIwzS6oMLIDsugXmMSzU2A1LL4R6JqfZ5peD-OEcQfkExH~Gcg91bgM--Qc-lSPQU2UmO8HJBwnHa3hY-6XIjXLYVFianr0tPPJgo6C0MQ__",
+    title: "Moana 2",
+    description: "Now Playing:",
+    score: "6.9/10",
+    content:
+      "After receiving an unexpected call from her wayfinding ancestors, Moana must journey to the far seas of Oceania and into dangerous, long-lost waters for an adventure unlike anything she's ever faced.",
+    trailer: "1",
+  },
+];
+
+// const data2 = [
+//   {
+//     poster_path:
+//       "https://s3-alpha-sig.figma.com/img/6415/2808/0ffbeccc1a7cdd3a5199e0755d66e253?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=fCQufcR3am1qiooLQaqM9Q41NfwFPOLbPAZzQ3vVA3ubkL8vXYVlOARoxW3mVGAKW10Or4rEsgbn1We-Uv7tTuXNoCKZySv1bpIdwl683L-HGFQCh2aolanzjTygbJRG8oxQTaOW~juemQwUKxKlVfDrku7eyKv7kCfb8GjtOtzOXMjPUGfQ1cPuDBT-lQ66x3KsLKXsEsvNyS04Nl-XgtKjiFq3J8vWz--Y7SKpB16xhs5medKqDLyR2gKclKWDTQHp2j3nb5xDgNcjumvVKF8BAMHiDyS~EKQBNdHZif7FuEWg4eIGY97oHzVI0GbCsUbWHh0mcj~FV-FZhmU~fQ__",
+//     title: "Dear Santa",
+//     vote_average: "6.9",
+//   },
+//   {
+//     poster_path:
+//       "https://s3-alpha-sig.figma.com/img/4b08/7764/bc03cf6c5fe844d5ce89f9658466dfef?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=VxBf6a0UyNNWI2Ef8E9MKlxZyc7jcRh-qGeLDOkVM5XhV1f~sVYxwSFoidjGrQXETt45YTjmIScYqZSvyuzwmi4qgBT5YAPnS9NSSjWBqWpSONfypvg3GL6l6SDn2mxOqJ1hlrtm6BSm8Dxh8Is5UH3D96O91AeEBtziVQkE8UXZxn51XGJbKR8haCM1Y9G~649PPFyWcnVO4BZHnDC31L8sm8a11VDPZDoo4LaP5cLeHgWkb4MO3DeHYsIqG-xMZ4B9yy5ffhL86UEuULXd5okztHFp1lEGdKTrARjAVR-4~5O5RGFjsm1Hr7G9ACqNPXJZD9dtt8ixIK8w0D7Zlw__",
+//     title: "Dear Santa",
+//     vote_average: "6.9",
+//   },
+// ];
+
+// const TitleType = () => {
+//   type TitleType = {
+//     adult: Boolean;
+//     backdrop_path: String;
+//     genre_ids: Array<Number>;
+//     id: Number;
+//     original_language: String;
+//     original_title: String;
+//     overview: String;
+//     popularity: Number;
+//     poster_path: String;
+//     release_date: String;
+//     title: String;
+//     video: Boolean;
+//     vote_average: Number;
+//     vote_count: Number;
+//   };
+// };
+export default function Home() {
+  const [data, setData] = useState([{}]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1&api_key=d67d8bebd0f4ff345f6505c99e9d0289"
+      )
+      .then((res) => setData(res.data.results))
+      .catch((err) => console.log(`error`, err));
+  }, []);
+
+  return (
+    <div className="w-screen h-screen">
+      <p>text</p>
+      <Carousel>
+        <CarouselContent>
+          {upcomingData.map((el, index) => (
+            <CarouselItem className="relative w-full h-full" key={index}>
+              <img className="w-full h-[600px]" src={el.image}></img>
+              <Card
+                className={
+                  "w-[404px] h-fit gap-4 absolute top-[178px] left-[140px] z-50 bg-transparent border-none"
+                }
+              >
+                <CardHeader>
+                  <CardDescription>{el.description}</CardDescription>
+                  <CardTitle>{el.title}</CardTitle>
+                  <CardDescription>{el.score}</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p className="w-[302px] text-[#FAFAFA] text-xs leading-[16px]">
+                    {el.content}
+                  </p>
+                </CardContent>
+                <CardFooter>
+                  <p>{el.trailer}</p>
+                </CardFooter>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="left-[44px]" />
+        <CarouselNext className="right-[44px]" />
+      </Carousel>
+      <div className="flex flex-wrap px-20 gap-[32px] ">
+        {data?.map((data: any, index: number) => {
+          return (
+            <Movies
+              key={index}
+              poster_path={`https://image.tmdb.org/t/p/original${data.poster_path}`}
+              title={data.title}
+              vote_average={data.vote_average}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Movies.tsx
@@ -0,0 +1,34 @@
+import { CardDescription, CardTitle } from "./ui/card";
+import { Star } from "lucide-react";
+
+export const Movies = ({
+  poster_path,
+  title,
+  vote_average,
+}: {
+  poster_path: string;
+  title: string;
+  vote_average: string;
+}) => {
+  return (
+    <div className="flex flex-col">
+      <img className="w-[230px] h-[340px]" src={poster_path}></img>
+      <div className="flex flex-col p-2 ">
+        <div className="flex gap-1">
+          <Star className="size-[16px] fill-[#FDE047] stroke-[#FDE047]" />
+          <div className="flex">
+            <CardDescription className="text-sm font-medium leading-[20px] text-[#09090B]">
+              {vote_average}{" "}
+            </CardDescription>
+            <p className=" flex items-center text-xs leading-[16px] text-[#71717A]">
+              /10
+            </p>
+          </div>
+        </div>
+        <CardTitle className="text-lg leading-[28px] text-ellipsis text-[#09090B] overflow-hidden">
+          {title}
+        </CardTitle>
+      </div>
+    </div>
+  );
+};
