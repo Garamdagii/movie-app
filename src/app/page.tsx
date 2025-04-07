3ccf1ce1dfbeea966162bbde6a4ff1719d3ff685
24LP7422 24LP7422 <24LP7422@G302-CC0115.local>
2025-04-07 09:57:57 +0800

    up3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,6 @@
 "use client";
 
 import { NowPlaying, SortedMovies } from "@/components";
-import {
-  Command,
-  CommandDialog,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-  CommandSeparator,
-  CommandShortcut,
-} from "@/components/ui/command";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { PopoverPortal } from "@radix-ui/react-popover";
 
 const data = [
   {
@@ -48,29 +31,5 @@ export default function Home() {
         })}
       </div>
     </div>
-    // <div>
-    //   <Command>
-    //     <CommandInput placeholder="Type a command or search..." />
-    //     <Popover>
-    //       <PopoverTrigger>
-    //         <CommandList>
-    //           <CommandEmpty>No results found.</CommandEmpty>
-    //           <CommandGroup heading="Suggestions">
-    //             <CommandItem>Calendar</CommandItem>
-    //             <CommandItem>Search Emoji</CommandItem>
-    //             <CommandItem>Calculator</CommandItem>
-    //           </CommandGroup>
-    //           <CommandSeparator />
-    //           <CommandGroup heading="Settings">
-    //             <CommandItem>Profile</CommandItem>
-    //             <CommandItem>Billing</CommandItem>
-    //             <CommandItem>Settings</CommandItem>
-    //           </CommandGroup>
-    //         </CommandList>
-    //       </PopoverTrigger>
-    //       <PopoverContent>Place content for the popover here.</PopoverContent>
-    //     </Popover>
-    //   </Command>
-    // </div>
   );
 }
--- src/components/label.tsx
+++ src/components/label.tsx
@@ -24,5 +24,3 @@ export const Label = ({
     </div>
   );
 };
-
-// currentpage === index
